449ce30e27d4872df8b8c92d39c08403d5e9b4d7
Prashant Raj <prashantraj01122@gmail.com>
2025-07-09 14:16:21 +0530

    url changes

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -6,7 +6,7 @@ export const userDataContext = createContext();
 
 
 const UserContext = ({ children }) => {
-  const serverURL = "https://my-voice-ai-backend.onrender.com"
+  const serverURL = "https://my-voice-ai-frontend.vercel.app/signup"
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
